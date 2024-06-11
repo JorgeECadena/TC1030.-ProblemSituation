affaa801db2cf489fd8a2fc33393e2d38f1f3133
Jorge Cadena <a01660252@tec.mx>
2024-06-11 13:40:25 -0600

    Corrected some documentation and added libraries

--- Series.cpp
+++ Series.cpp
@@ -1,4 +1,8 @@
 #include ".\Series.h"
+#include ".\Episode.h"
+#include <iostream>
+#include <string>
+#include <vector>
 
 /*
 * Default constructor, sets every attribute as a NULL value
@@ -9,7 +13,7 @@ Series :: Series() : Video(), votes(NULL), episodes(NULL) {
 
 /*
 * Parameterized constructor, receives int id, string name, float length, 
-* string genre to set the class attributes
+* string genre, int votes, vector<Episode> episodes to set the class attributes
 */
 Series :: Series(int id, std::string name, float length, std::string genre, int votes, std::vector<Episode> episodes) :
 Video(id, name, length, genre), votes(votes), episodes(episodes) {
@@ -24,14 +28,14 @@ Series :: ~Series() {
 }
 
 /*
-* Returns class int id
+* Returns class int votes
 */
 int Series :: getVotes(void) {
     return votes;
 }
 
 /*
-* Returns class string name
+* Returns class vector<Episode> episodes
 */
 std::vector<Episode> Series :: getEpisodes(void) {
     return episodes;
@@ -39,14 +43,14 @@ std::vector<Episode> Series :: getEpisodes(void) {
 
 
 /*
-* Set the id attribute value to the given int value
+* Set the votes attribute value to the given int value
 */
 void Series :: setVotes(int votes) {
     this -> votes = votes;
 }
 
 /*
-* Set the name attribute value to the given string value
+* Set the episodes attribute value to the given vector<Episode> value
 */
 void Series :: setEpisodes(std::vector<Episode> episodes) {
     this -> episodes = episodes;
