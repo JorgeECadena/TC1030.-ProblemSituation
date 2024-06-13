68d7f937978f3bb09400315b415956e363fdd0e9
Jorge Cadena <a01660252@tec.mx>
2024-06-13 17:22:08 -0600

    Added string library

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include ".\Video.h"
 #include ".\Movie.h"
 #include ".\Series.h"
 #include ".\Episode.h"
 
-int main() {
-    
+int main() {    
 
     return 0;
 }
